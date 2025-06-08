94b66b3989bf32f4c17d2ed1f396ac924cc0f6a9
sh5452 <105583484+sh5452@users.noreply.github.com>
2025-06-08 22:07:38 +0300

    a

---
+++ db/knex.js
@@ -0,0 +1,16 @@
+const knex = require('knex');
+const config = require('con');
+
+const connectedKnex = knex({
+    client: 'pg',
+    version: '16',
+    connection: {
+        host: config.db_cloud.host,
+        user: config.db_cloud.user,
+        password: config.db_cloud.password,
+        database: config.db_cloud.database,
+        ssl: true
+    }
+});
+
+module.exports = connectedKnex;
